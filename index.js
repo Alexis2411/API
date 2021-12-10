1a1abbb466c6a6537f5278bf70b3f9ce0e55462a
ALEXIS JAVIER ALVAREZ ROMAN <72472559+Alexis2411@users.noreply.github.com>
2021-12-09 23:08:13 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 const server = require('./server');
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8126;
 
-server.listen(PORT, ()=> console.log('Server is live at localhost: 8080'));
+server.listen(PORT, ()=> console.log('Server is live at localhost: 8126'));
